c40e20bd8168a137b4efa3eef83af3891fa67a98
casey-minzi <cluvhope@gmail.com>
2020-04-30 19:43:28 +0100

    Added Clock

---
+++ clock.js
@@ -0,0 +1,21 @@
+const clockContainer = document.querySelector('.js-clock');
+const clockTitle = clockContainer.querySelector('.js-title');
+
+const normaliseTimeToken = (token) => {
+    return token < 10 ? `0${token}` : token;
+};
+
+const getTime = () => {
+    const date = new Date();
+    const hour = normaliseTimeToken(date.getHours());
+    const min = normaliseTimeToken(date.getMinutes());
+    const sec = normaliseTimeToken(date.getSeconds());
+    clockTitle.innerText = `${hour}:${min}:${sec}`;
+};
+
+function init() {
+    getTime();
+    setInterval(getTime, 1000);
+}
+
+init();
