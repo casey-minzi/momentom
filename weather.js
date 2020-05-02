32d758abe2a0ae23fce540a05fa0181bbd5e6f2d
casey-minzi <cluvhope@gmail.com>
2020-05-02 19:23:23 +0100

    Added Weather and Place

---
+++ weather.js
@@ -0,0 +1,47 @@
+const weather = document.querySelector('.js-weather');
+const COORDS = 'coords';
+const API_KEY = 'd659975cd487bfeaa275546a8bfd1923';
+
+function saveCoords(coords) {
+    localStorage.setItem(COORDS, JSON.stringify(coords));
+}
+
+function getWeather(lat, lon) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`)
+        .then(res => res.json())
+        .then(json => {
+            const temp = json.main.temp;
+            const place = json.name;
+            const country = json.sys.country;
+            weather.innerText = `${temp}℃ @ ${place}, ${country}`;
+        });
+}
+
+function handleGeoSuccess(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = { latitude, longitude };
+    saveCoords(coordsObj);
+    getWeather(latitude, longitude);
+}
+
+function handleGeoError() {
+    console.log('Location not allowed');
+}
+
+function askForCoords() {
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadWeather() {
+    const loadedCoords = JSON.parse(localStorage.getItem(COORDS));
+    loadedCoords ?
+        getWeather(loadedCoords.latitude, loadedCoords.longitude) :
+        askForCoords();
+}
+
+function init() {
+    loadWeather();
+}
+
+init();
